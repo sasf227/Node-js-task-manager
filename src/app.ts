34e7a48d9dc65c53b67529bb723747a7d9230a9f
sasf227 <131782371+sasf227@users.noreply.github.com>
2026-03-11 15:50:51 +0100

    some front end

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import passHash from './passhash.ts'
-import type authetnicate_user_signup_request from './schemas.ts'
+import type {authetnicate_user_signup_request, keys, err} from './schemas.ts'
+import { errorMonitor } from 'node:events';
+
+
+
+
 
 const app: express.Application = express();
 const port: number = 3000;
@@ -25,15 +30,37 @@ app.get('/signup', (_req, _res) => {
 
 app.post('/signup', (_req, _res) => {
     const body: authetnicate_user_signup_request = _req.body;
-    if(Number.isNaN(body.name) || Number.isNaN(body.email) ) {
-        _res.send({error: 'Name or email cannot be just a number'})
-    }
+    const b_keys: keys<authetnicate_user_signup_request> = ['name', 'email', 'password', 'confirm_password'];
+    const isNum = !(isNaN(Number(body.name)) || isNaN(Number(body.email)));
+    const pwd_match = body.password !== body.confirm_password;
+    let error: err = {
+            num_error: '',
+            empty_error: '',
+            pwd_error: '',
+        };
+
+    for (const key of b_keys) {
+        if (!(key in body) ) {
+            _res.status(400).send();
+        } else if (!body[key]) {
+            error.empty_error=`Field ${key} cannot be empty!`;
+            _res.send(error);
+        };
+    };
+
+    if (isNum) {
+            error.num_error = 'Name or email cannot be a number!' 
+            _res.send(error);
+        } else if (pwd_match) {
+            error.pwd_error = 'Passwords does not matches'
+            _res.send(error);
+    };
+    _res.send({url: '/home'})
     
-    // for (const [key, value] of Object.entries(body)) {
-    //     console.log(key, value, Number(value))
-    // }
-    console.log(body, Number(body.name), Number(body.email), Number(body.password), Number(body.confirm_password))
-    
+});
+
+app.get('/home', (_req, _res) => {
+    _res.render('home')
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/schemas.ts
+++ src/schemas.ts
@@ -1,7 +1,14 @@
-export default interface authetnicate_user_signup_request {
+export type authetnicate_user_signup_request = {
     name: string;
     email: string;
     password: string;
     confirm_password: string;
+}
+export type keys<T> = Array<keyof T>
+
+export type err = {
+    num_error: string;
+    empty_error: string;
+    pwd_error: string;
 }
 
