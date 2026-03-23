ef15256146444e063ead48ddd84d6dd829198cdd
sasf227 <131782371+sasf227@users.noreply.github.com>
2026-03-23 13:12:39 +0100

    jwt

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import login_Check from './check.ts';
 import dbOperations from './db_operations.ts';
 import Authenticate from './authenticate.ts';
+import jwtAuthorization from './jwt.ts'
 import passHash from './passhash.ts'
 import type {authenticate_user_signup_request, authenticate_user_login_reques, user_db_schema, keys} from './schemas.ts'
 import bodyParser from 'body-parser';
@@ -9,10 +10,13 @@ import {pool} from './db_connect.ts'
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import { v4 } from 'uuid';
-import { error } from 'node:console';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import type { constants } from 'node:buffer';
 
 
-export const app: express.Application = express();
+const app: express.Application = express();
+dotenv.config();
 const port: number = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -30,6 +34,15 @@ app.get('/', (_req, _res) => {
     _res.render('index', {result: null});
 });
 
+app.post("/user/generateToken", async function n(_req, _res,) {
+
+});
+
+app.get("/user/validateToken", (_req, _res) => {
+
+});
+
+
 app.get('/login', (_req, _res) => {
     _res.render('login', {result: null});
 });
@@ -44,6 +57,7 @@ app.post('/login', express.urlencoded({extended: true}), async function login(_r
     const login = new login_Check(_req, _res, body);
     const db = new dbOperations;
     const pwd = new passHash;
+    //const jwt = new jwtAuthorization(_req, _res)
 
     //db operation
     const result = await db.getByValue<user_db_schema> ('users', ['email'], body.email);
@@ -87,10 +101,17 @@ app.post('/signup', async function signup(_req, _res) {
     const keys: keys<authenticate_user_signup_request> = ['name', 'email', 'password', 'confirm_password'];
     const num_keys = ['name', 'email'];
     const uuid = v4();
+    const user_data= {
+        username: body.name,
+        email: body.email,
+        uuid: uuid,
+    }
+
     // classes
     const signup = new login_Check(_req, _res, body);
     const db = new dbOperations;
     const pwd = new passHash;
+    const jwt = new jwtAuthorization(_req, _res, 'gfg_jwt_secret_key', 'gfg_token_header_key');
 
     // frontend(right user data) check
     signup.signup(keys, num_keys, 'name', 'email', 'password', 'confirm_password')
@@ -99,13 +120,22 @@ app.post('/signup', async function signup(_req, _res) {
     const hashed_pwd = await pwd.hashPassword(body.password);
 
     //db operation
-    db.insertInto<user_db_schema> ('users', ['username', 'email', 'password', 'uuid'], [body.name, body.email, hashed_pwd, uuid,]);
-    const result = await db.getByValue<user_db_schema> ('users', ['email'], body.email);
-    const user = result.rows[0];
+    try {
+        const result = await db.insertInto<user_db_schema> ('users', ['username', 'email', 'password', 'uuid'], [body.name, body.email, hashed_pwd, uuid,]);
+    } catch (error) {
+        if (error) {
+            _res.send({error: "Unknownn error try again later!"})
+        };
+    };
+    
+    const check_result = await db.getByValue<user_db_schema> ('users', ['email'], body.email);
+    const user = check_result.rows[0];
+    
+    
 
     // setting the cookies 
     if (!user) {
-        _res.send({error: "Couldn't create a user, try again later!"})
+        _res.send({error: "Couldn't create a user, try again later!"});
     } else {
         _req.session.user = {
             uuid: uuid,
@@ -113,7 +143,7 @@ app.post('/signup', async function signup(_req, _res) {
             email: body.email,
         };
         _res.cookie("sessionID", _req.sessionID);
-
+        jwt.signJWT(user_data, 1, "h")
         // redirecting home
         _res.send({url: '/home'})
     };
---
+++ src/jwt.ts
@@ -0,0 +1,51 @@
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+import { error } from 'node:console';
+
+export default class jwtAuthorization {
+    req: any;
+    res: any;
+    jwtSecretKey: string;
+    tokenHeaderKey: string;
+    constructor(req: any, res: any, jwtSecretKey: string, tokenHeaderKey: string) {
+        this.req = req;
+        this.res = res;
+        this.jwtSecretKey = jwtSecretKey;
+        this.tokenHeaderKey = tokenHeaderKey;
+    }
+    async signJWT<T extends object> (data: T | Record<string, any>, exp: number = 1, timetype: "h" = "h") {
+        const token = jwt.sign(data, this.jwtSecretKey, {expiresIn: `${exp}${timetype}`})
+        this.res.cookie("JWT", token);
+    };
+
+    async verifyJWT(): Promise<boolean> {
+        const token = this.req.cookies['JWT'];
+        if (!token) {
+            return this.res.status(401).send('Unauthorized');
+        } else {
+            try {
+                const verifed = jwt.verify(token, this.jwtSecretKey);
+                if (verifed) {
+                    return true
+                } else {
+                    return this.res.status(401).send('Unauthorized');
+                };
+                } catch (error) {
+                    return this.res.status(401).send('Unauthorized');
+            };
+        };
+    };
+
+    async decodeJWT (): Promise<string | jwt.JwtPayload | null> {
+        const token = this.req.cookies['JWT'];
+        if (!token) {
+            return this.res.status(401).send('Unauthorized');
+        } else {
+            try {
+                const decoded_result = jwt.decode(token)
+                return decoded_result;
+            } catch (error) {
+                return this.res.status(401).send('Unauthorized');
+            };
+        };
+    };
+};
